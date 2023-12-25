449ab921cd2fc807a98e1afe94b7f013e3820fb9
aleks-PC <aleksejvasilev756@mail.ru>
2023-12-25 19:41:29 +0500

    add endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 import { IStroies } from './interface/stories.interface';
 
 
-@Controller()
+@Controller('stories')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
